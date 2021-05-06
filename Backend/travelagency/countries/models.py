1f36387bfa5a68fa518c651e5415e1fb661ae1d8
José Cuevas <jose.cuevas.cv@gmail.com>
2021-05-05 19:49:59 -0500

    Backend Admin Model
    Backend Admin Model

--- Backend/travelagency/countries/models.py
+++ Backend/travelagency/countries/models.py
@@ -1,3 +1,16 @@
 from djongo import models
 
 # Create your models here.
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    currency = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=100)
+    centseparator = models.CharField(max_length=100)
+    millionseparator = models.CharField(max_length=100)
+    locale = models.CharField(max_length=100)
+    decimals = models.CharField(max_length=100)
+    displaysymbol = models.CharField(max_length=100)
+
+ 
