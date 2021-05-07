c392b733a5eea03f2ccd294f212f35ea376e06b2
José Cuevas <jose.cuevas.cv@gmail.com>
2021-05-06 22:09:26 -0500

    fix crud

--- Backend/travelagency/countries/api.py
+++ Backend/travelagency/countries/api.py
@@ -2,18 +2,31 @@ from rest_framework import generics
 from rest_framework.response import Response
 from .serializer import CountrySerializer
 from .models import Country
+
+
 class CountryCreateApi(generics.CreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+
 
 class CountryApi(generics.ListAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
+
+class CountryFindApi(generics.RetrieveAPIView):
+    queryset = Country.objects.all()
+    lookup_field = 'name'
+    serializer_class = CountrySerializer
+
+
 class CountryUpdateApi(generics.RetrieveUpdateAPIView):
     queryset = Country.objects.all()
+    lookup_field = 'name'
     serializer_class = CountrySerializer
+
 
 class CountryDeleteApi(generics.DestroyAPIView):
     queryset = Country.objects.all()
+    lookup_field = 'name'
     serializer_class = CountrySerializer
